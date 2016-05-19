d91e7ab6dbcaa0d8f91fb748c7a399cb8ba7d539
Gregory Wehmeier <wehmenstein@gmail.com>
2016-05-19 02:11:37 -0500

    add test ajax call

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  console.log(test());
+})
+
+function test(){
+  $.ajax({
+    method: 'GET'
+    url: 'http://destroyed.herokuapp.com/films'
+  })
+  .done(function(response){
+    return response;
+  })
+  .error(function(xhr, unknown, error){
+    return error;
+  })
+}
